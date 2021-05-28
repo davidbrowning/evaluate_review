e0930f717f9bdd94c682ddc95ae6b3594a4f5146
Dave Browning <dmbrowning21@gmail.com>
2021-05-28 23:13:10 +0000

    updated tests

--- test/evaluate_review_test.exs
+++ test/evaluate_review_test.exs
@@ -1,18 +1,44 @@
 defmodule EvaluateReviewTest do
   use ExUnit.Case
-
-  @tag needs_cache: true
   doctest EvaluateReview
 
-  @tag needs_cache: true
+  test "archive.org ok" do
+    url = "https://archive.org"
+    {:ok, response} = HTTPoison.get(url)
+    assert response.status_code == 200
+  end
+
+  test "way back machine ok" do
+    url = "https://web.archive.org"
+    {:ok, response} = HTTPoison.get(url)
+    assert response.status_code == 200
+  end
+
+  test "way back machine dealerrater.com" do
+    url =
+      "http://archive.org/wayback/available?url=https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+
+    cache_file_wayback_status = ".cache/wayback.status.json"
+    {:ok, response} = HTTPoison.get(url)
+    {:ok, body} = Jason.decode(response.body)
+    assert body["archived_snapshots"]["closest"]["available"] == true
+    assert body["archived_snapshots"]["closest"]["status"] == "200"
+    {:ok, cache} = File.open(cache_file_wayback_status, [:write])
+    IO.binwrite(cache, Jason.encode!(body["archived_snapshots"]["closest"]))
+    assert File.exists?(cache_file_wayback_status)
+  end
+
   test "review cache" do
     cache_file_wayback_status = ".cache/wayback.status.json"
-    assert File.exists?(cache_file_wayback_status)
-    {:ok, wayback_status} = EvaluateReview.read_json(cache_file_wayback_status)
-
-    url =
-      "https://web.archive.org/web/#{wayback_status["timestamp"]}/https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
-
+    url = if(File.exists?(cache_file_wayback_status)) do
+        {:ok, wayback_status} = EvaluateReview.read_json(cache_file_wayback_status)
+        t_url = "https://web.archive.org/web/#{wayback_status["timestamp"]}/https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+        t_url
+    else
+        t_url = "https://web.archive.org/web/20201127110830/https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+        IO.puts("still writing cache, manually setting last known archive.org date")
+        t_url
+    end
     cache_file_wayback_reviews = ".cache/wayback.reviews.bin"
     review_list = EvaluateReview.scrape(url)
     EvaluateReview.cache(review_list, cache_file_wayback_reviews)
@@ -22,21 +48,27 @@ defmodule EvaluateReviewTest do
   @tag needs_cache: true
   test "load review cache" do
     cache_file_wayback_reviews = ".cache/wayback.reviews.bin"
-    assert File.exists?(cache_file_wayback_reviews)
-    review_list = EvaluateReview.load_from_cache(cache_file_wayback_reviews)
-    assert is_list(review_list)
-    assert is_tuple(hd(review_list))
+    if(File.exists?(cache_file_wayback_reviews)) do
+      review_list = EvaluateReview.load_from_cache(cache_file_wayback_reviews)
+      assert is_list(review_list)
+      assert is_tuple(hd(review_list))
+    else
+      raise "cached file not present: #{cache_file_wayback_reviews}"
+    end
   end
 
-  @tag needs_cache: true
   test "scrape review" do
     cache_file_wayback_status = ".cache/wayback.status.json"
-    assert File.exists?(cache_file_wayback_status)
-    {:ok, wayback_status} = EvaluateReview.read_json(cache_file_wayback_status)
-    IO.puts("timestamp of latest archive: #{wayback_status["timestamp"]}")
-
-    url =
-      "https://web.archive.org/web/#{wayback_status["timestamp"]}/https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+    url = if(File.exists?(cache_file_wayback_status)) do
+        {:ok, wayback_status} = EvaluateReview.read_json(cache_file_wayback_status)
+        t_url = "https://web.archive.org/web/#{wayback_status["timestamp"]}/https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+        IO.puts("timestamp of latest archive: #{wayback_status["timestamp"]}")
+        t_url
+    else
+        t_url = "https://web.archive.org/web/20201127110830/https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+        IO.puts("still writing cache, manually setting last known archive.org date")
+        t_url
+    end
 
     # live
     # url =
@@ -49,11 +81,14 @@ defmodule EvaluateReviewTest do
   @tag needs_cache: true
   test "top three offenders" do
     cache_file_wayback_reviews = ".cache/wayback.reviews.bin"
-    assert File.exists?(cache_file_wayback_reviews)
-    review_list = EvaluateReview.load_from_cache(cache_file_wayback_reviews)
-    assert is_list(review_list)
-    assert is_tuple(hd(review_list))
-    top3 = EvaluateReview.suspect_reviews(review_list)
-    assert length(top3) == 3
+    if(File.exists?(cache_file_wayback_reviews)) do
+      review_list = EvaluateReview.load_from_cache(cache_file_wayback_reviews)
+      assert is_list(review_list)
+      assert is_tuple(hd(review_list))
+      top3 = EvaluateReview.suspect_reviews(review_list)
+      assert length(top3) == 3
+    else
+      raise "cached file not present: #{cache_file_wayback_reviews}"
+    end
   end
 end
